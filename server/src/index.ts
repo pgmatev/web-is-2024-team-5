34ead40a765e40cfb3bef26bb9d6de9dd6af0eb9
pgmatev <petargabriel.matev@gmail.com>
2024-04-25 20:32:02 +0300

    added simple fileupload endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,27 +1,54 @@
-import { knex } from "knex";
-import { Model } from "objection";
-import knexConfig from "../knexfile";
 import { config } from "./config";
 
 import express, { json } from "express";
-import http from "http";
-
-//Connection to the database
-const knexClient = knex(knexConfig.development);
-Model.knex(knexClient);
+import multer from "multer";
+import cors from "cors";
 
 //Server
 const app = express();
 const port = config.get("port");
 
 app.use(json());
+app.use(cors());
 
 app.use((req, res, next) => {
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
   next();
 });
 
-app.use("/", (_req, res) => res.send("Hello world"));
+app.get("/hello", (_req, res) => res.send("Hello world"));
 
-app.listen(port);
-console.log("Server started on port ", port);
+/**
+ * When working with HTTP, 
+ * usually the `multipart/form-data` encoding is used for files. 
+ * That is because it can handle binary data and file metadata (file name, etc),
+ * something that cannot be transferred in plain text
+ */
+
+
+//File uploading
+// to System Storage
+const upload = multer({ dest: "uploads/" });
+
+// to Memory Buffer
+// const upload = multer({
+//   storage: multer.memoryStorage()
+// });
+
+// Multer middlewares process multipart/form-data
+app.post("/file", upload.single('file'), (req, res) => {
+  const file = req.file;
+
+  if (file) {
+    res.json({
+      fileName: file.originalname,
+      fileSize: file.size,
+    });
+  } else {
+    res.status(400).send("No file uploaded");
+  }
+});
+
+app.listen(port, () => {
+  console.log("Server started on port ", port);
+});
