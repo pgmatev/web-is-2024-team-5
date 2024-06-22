20b4d4c7c3c8b9edf7b95efaae4ca9fa11693aa7
Nikola <nkl_andonov@yahoo.com>
2024-06-22 19:58:32 +0300

    password validation; list of form field errors returned on invalid form input

--- server/src/services/UserService.ts
+++ server/src/services/UserService.ts
@@ -2,8 +2,8 @@ import {z, ZodError} from "zod";
 import { IUser, User } from "../models/UserModel";
 
 export const CreateUserSchema = z.object({
-  email: z.string().email({message: "Email is required"}),
-  username: z.string(),
+  email: z.string().email({message: "Please enter a valid email format."}),
+  username: z.string().min(1, {message: "Please enter a username."}),
   password: z.string()
       .min(6, {message: "Password must be at least 6 characters."})
       .regex(/[A-Z]/, { message: "Password must contain at least one uppercase letter." })
