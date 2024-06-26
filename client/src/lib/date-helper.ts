3e29759c24034ccdf284698c40d71fb42f5530f4
pgmatev <petargabriel.matev@gmail.com>
2024-06-26 22:56:16 +0300

    date improvements

---
+++ client/src/lib/date-helper.ts
@@ -0,0 +1,16 @@
+export function formatDate(date: Date) {
+  const today = new Date();
+  if (
+    date.getDate() === today.getDate() &&
+    date.getMonth() === today.getMonth() &&
+    date.getFullYear() === today.getFullYear()
+  ) {
+    // Display hours and minutes
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
+  } else {
+    // Display full date
+    return date.toLocaleDateString();
+  }
+}
