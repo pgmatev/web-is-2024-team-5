c6447b25896d71e73a2c42fecc60b58eb80c2c00
VelislavaVarbanova <velislava.varbanova@gmail.com>
2024-06-26 03:36:05 +0300

    added dropdowns to the chat settings page

--- client/src/components/ChatSettings/ChatSettings.tsx
+++ client/src/components/ChatSettings/ChatSettings.tsx
@@ -1,35 +1,105 @@
-import { RiArrowRightLine, RiArrowRightSLine, RiEditLine } from "@remixicon/react";
+import { RiArrowDownSLine, RiArrowRightSLine, RiEditLine, RiForbid2Fill, RiVolumeMuteFill } from "@remixicon/react";
 import styles from "./ChatSettings.module.css";
-
-interface ChatSettingsProps {
-    onOpenSettings: () => void;
-}
+import { useCallback, useMemo, useState } from "react";
 
 const ChatInfo = {
     name: "Group 1",
     members: ["Ivan Ivanov", "George Petrov", "Martin Iliev"]
 }
 
+
+interface ChatSettingsProps {
+    onOpenSettings: () => void;
+    // onOpenChatMembers: () => void;
+    // onOpenChatFiles: () => void;
+    // onOpenChatSecutity: () => void;
+}
+
 export function ChatSettings({onOpenSettings}:ChatSettingsProps) {
+    
+    const [isChatMembersOpen, setIsChatMembersOpen] = useState(false);
+    const onOpenChatMembers = useCallback(() => {
+        setIsChatMembersOpen(!isChatMembersOpen);
+    }, [isChatMembersOpen]);
+
+    const renderChatMembers = useMemo(() => {
+        if(isChatMembersOpen) {
+        return <ul className={styles["chat-members-ul"]}>
+            <li>
+                <h3>Ivan Ivanov</h3>
+            </li>
+            <li>
+                <h3>George Petrov</h3>
+            </li>
+        </ul>;
+        }
+        return ;
+    }, [isChatMembersOpen]);
+
+    //---------------------------------------------
+    const [isOpenChatFiles, setIsOpenChatFiles] = useState(false);
+    const onOpenChatFiles = useCallback(() => {
+        setIsOpenChatFiles(!isOpenChatFiles);
+    }, [isOpenChatFiles]);
+
+    const renderChatFiles = useMemo(() => {
+        if(isOpenChatFiles) {
+        return <h3 className={styles["media-files-h3"]}>There are no media and files</h3> ;
+        }
+        return ;
+    }, [isOpenChatFiles]);
+    //--------------------------------------------
+    
+    const [isOpenChatSecutity, setIsOpenChatSecutity] = useState(false);
+    const onOpenChatSecutity = useCallback(() => {
+        setIsOpenChatSecutity(!isOpenChatSecutity);
+    }, [isOpenChatSecutity]);
+
+    const renderChatSecurity = useMemo(() => {
+        if(isOpenChatSecutity) {
+        return <ul className={styles["chat-security-ul"]}>
+                    <li>
+                        <RiVolumeMuteFill></RiVolumeMuteFill>
+                        <h3>Mute chat</h3>
+                    </li>
+                    <li>
+                        <RiForbid2Fill></RiForbid2Fill>
+                        <h3>Leave chat</h3>
+                    </li>
+                </ul>;
+        }
+        return ;
+    }, [isOpenChatSecutity]);
+    //------------------------------------------------------
+
     return ( 
     <>
-        <h1 className={styles["chat-name"]}>{ChatInfo.name}</h1>
+        <input disabled value={ChatInfo.name} className={styles["chat-name"]}></input>
         <div className={styles["edit-chat-name"]}>
             <RiEditLine className={styles["ri-edit-line"]}></RiEditLine>
             <h2>Change group name</h2>
         </div>
-        <div className={styles["chat-members"]}>
+        <button className={styles["chat-members"]} onClick={ () => onOpenChatMembers()}>
             <h3>Chat members</h3>
-            <RiArrowRightSLine></RiArrowRightSLine>
-        </div>
-        <div className={styles["chat-files"]}>
+            {isChatMembersOpen
+            ?<RiArrowDownSLine></RiArrowDownSLine> 
+            :<RiArrowRightSLine></RiArrowRightSLine>}
+        </button>
+        {renderChatMembers}
+        <button className={styles["chat-files"]} onClick={ () => onOpenChatFiles()}>
             <h3>Media and files</h3>
-            <RiArrowRightSLine></RiArrowRightSLine>
-        </div>
-        <div className={styles["chat-security"]}>
+            {isOpenChatFiles 
+            ? <RiArrowDownSLine></RiArrowDownSLine> 
+            : <RiArrowRightSLine></RiArrowRightSLine>}
+        </button>
+        {renderChatFiles}
+        <button className={styles["chat-security"]} onClick={onOpenChatSecutity} >
             <h3>Security</h3>
-            <RiArrowRightSLine></RiArrowRightSLine>
-        </div>
+            {isOpenChatSecutity
+            ? <RiArrowDownSLine></RiArrowDownSLine> 
+            : <RiArrowRightSLine></RiArrowRightSLine>}
+        </button>
+        {renderChatSecurity}
     </>
     );
 }
